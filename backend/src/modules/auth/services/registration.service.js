70b966dcdbe6399a66598ba9967b448ea0299a4d
shaimaragab <shaimaragab2017@gmail.com>
2025-07-05 02:39:50 +0300

    signup function

--- backend/src/modules/auth/services/registration.service.js
+++ backend/src/modules/auth/services/registration.service.js
@@ -1,12 +1,27 @@
+import { userModel } from "../../../db/model/user.model.js";
+import { asyncHandler } from "../../../utils/response/error.response.js";
+import { successResponse } from "../../../utils/response/sucess.response.js";
+import { generateHash } from "../../../utils/security/hash.security.js";
 
 
 
 
 
-const signup =(req,res, next) => {
-    return res.json({ message: "signup" , Users})
-}
-
+const signup = asyncHandler(async (req, res, next) => {
+  const { username, email, password, phone } = req.body;
+  console.log(username, email, password, phone);
+  const checkUser = await userModel.findOne({ email });
+  if (checkUser) return next(new Error("email already exist", { cause: 409 }));
+  const hashedPassword = generateHash({ plainText: password });
+  const user = await userModel.create({
+    username,
+    email,
+    password: hashedPassword,
+    phone,
+  });
+  //emailEvent.emit("sendConfirmEmail", { id: user._id, email });
+  return successResponse({ res, status: 201, data: { user } });
+});
 const confirmEmail =(req,res, next) => {
 return res.json({message: "confirmEmail"})
 }
