cff2b6e06deac2b78ec3ec1500a342fa73b024ba
shaimaragab <shaimaragab2017@gmail.com>
2025-07-08 22:35:12 +0300

    refresh token

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {signup , confirmEmail} from "./services/registration.service.js";
-import { login } from "./services/login.service.js";
+import { login, refreshToken } from "./services/login.service.js";
 import { validation } from "../../middleware/validation.middleware.js";
 import * as validators from "../auth/auth.validation.js"
 const router = Router()
@@ -10,5 +10,6 @@ const router = Router()
 router.post("/signup",validation(validators.signup),signup)
 router.patch("/confirm-email", validation(validators.confirmEmail),confirmEmail)
 router.post("/login",validation(validators.login),login)
+router.get("/refresh-token",refreshToken)
 export default router;
 
--- backend/src/modules/auth/services/login.service.js
+++ backend/src/modules/auth/services/login.service.js
@@ -3,7 +3,7 @@ import * as dbService from "../../../db/db.services.js"
 import { asyncHandler } from "../../../utils/response/error.response.js";
 import { successResponse } from "../../../utils/response/success.response.js";
 import { compareHash } from "../../../utils/security/hash.security.js";
-import { generateToken } from "../../../utils/security/token.security.js";
+import { decodeToken, generateToken, tokenTypes } from "../../../utils/security/token.security.js";
 
 export const login = asyncHandler(async (req,res,next)=>{
   
@@ -11,7 +11,7 @@ export const login = asyncHandler(async (req,res,next)=>{
     console.log(email,password);
     const user = await dbService.findOne({
           model: userModel,
-          filter: { email, isDeleted:{$exists:false} },
+          filter: { email, isDeleted:false },
         });//object or null
     if(!user) return next(new Error("invalid account",{cause:404}))
         console.log(user.password);
@@ -20,14 +20,20 @@ export const login = asyncHandler(async (req,res,next)=>{
     if(!compareHash({plainText:password,hashedValue:user.password}) ) 
         return next(new Error("invalid account credentials ",{cause:404}))
     console.log(user.isDeleted);
-    
-    if (user.isDeleted) {
-        return next(new Error("account freezed please reactivate it",{cause:400}))
-    }
-    
     const accessToken=generateToken({payload:{id:user._id},signature:user.role == roleTypes.admin?process.env.SYSTEM_ACCESS_TOKEN: process.env.USER_ACCESS_TOKEN})
     const refreshToken=generateToken({payload:{id:user._id},signature:user.role == roleTypes.admin?process.env.SYSTEM_REFRESH_TOKEN: process.env.USER_REFRESH_TOKEN,expiresIn:31536000})
     console.log(accessToken,refreshToken);
     return successResponse({res,message:"Done",status:200,data:{accessToken,refreshToken}})     
+   
+})
+export const refreshToken = asyncHandler(async (req,res,next)=>{
+        
+    const {authorization} = req.headers
+    console.log(authorization);
+    if(!authorization)  return next(new Error("authorization required",{cause:400}))
+    const user = await decodeToken({authorization,tokenType:tokenTypes.refresh})
+    const accessToken=generateToken({payload:{id:user._id},signature:user.role == roleTypes.admin?process.env.SYSTEM_ACCESS_TOKEN: process.env.USER_ACCESS_TOKEN})
+    const refreshToken=generateToken({payload:{id:user._id},signature:user.role == roleTypes.admin?process.env.SYSTEM_REFRESH_TOKEN: process.env.USER_REFRESH_TOKEN,expiresIn:31536000})
+    return successResponse({res,message:"Done",status:200,data:{accessToken,refreshToken}})  
    
 })
